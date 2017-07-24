7bf1c3f63cbd1903e27defbfb65b49434e50081f
simzipark <bzh0406@naver.com>
2017-07-24 14:31:11 +0900

    using Router

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,36 +1,15 @@
 from django.conf.urls import url, include  
-from rest_framework.urlpatterns import format_suffix_patterns  
-from snippets import views
-from snippets.views import SnippetViewSet, UserViewSet, api_root  
-from rest_framework import renderers
+from snippets import views  
+from rest_framework.routers import DefaultRouter
 
+# 라우터를 생성하고 뷰셋을 등록합니다
+router = DefaultRouter()  
+router.register(r'snippets', views.SnippetViewSet)  
+router.register(r'users', views.UserViewSet)
 
-snippet_list = SnippetViewSet.as_view({  
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({  
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({  
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({  
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({  
-    'get': 'retrieve'
-})
-
-
-urlpatterns = format_suffix_patterns([  
-    url(r'^$', api_root),
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-])
+# 이제 API URL을 라우터가 자동으로 인식합니다
+# 추가로 탐색 가능한 API를 구현하기 위해 로그인에 사용할 URL은 직접 설정을 했습니다
+urlpatterns = [  
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
