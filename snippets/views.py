0d7983d2c764397205ce1e9c6cc19d191e69bcb7
simzipark <bzh0406@naver.com>
2017-07-24 14:29:23 +0900

    using ViewSet

--- snippets/views.py
+++ snippets/views.py
@@ -8,7 +8,10 @@ from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view  
 from rest_framework.response import Response  
 from rest_framework.reverse import reverse
-from rest_framework import renderers  
+from rest_framework import renderers 
+from rest_framework import viewsets 
+from rest_framework.decorators import detail_route
+
 
 class SnippetHighlight(generics.GenericAPIView):  
     queryset = Snippet.objects.all()
@@ -25,30 +28,31 @@ def api_root(request, format=None):
         'snippets': reverse('snippet-list', request=request, format=format)
     })
 
-class UserList(generics.ListAPIView):  
+class UserViewSet(viewsets.ReadOnlyModelViewSet):  
+    """
+    이 뷰셋은 `list`와 `detail` 기능을 자동으로 지원합니다
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):  
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class SnippetViewSet(viewsets.ModelViewSet):  
+    """
+    이 뷰셋은 `list`와 `create`, `retrieve`, `update`, 'destroy` 기능을 자동으로 지원합니다
 
-
-class SnippetList(generics.ListCreateAPIView):  
+    여기에 `highlight` 기능의 코드만 추가로 작성했습니다
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)  
-    def perform_create(self, serializer):  
-    	serializer.save(owner=self.request.user)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):  
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,  
-                      IsOwnerOrReadOnly,)
-
+    def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
